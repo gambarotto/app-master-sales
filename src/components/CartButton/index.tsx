80e9aeb7426d2b75993f3d29ee6562a515e3bd94
gambarotto <dgcdeveloper@gmail.com>
2021-07-08 18:37:00 -0300

    update cardButton

--- src/components/CartButton/index.tsx
+++ src/components/CartButton/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import themeGlobal from '../../styles/global';
 import { Container, ContainerBadge, IconCart, TextBadge } from './styles';
@@ -6,15 +7,22 @@ interface ICartButtonProps {
   qtyCartProducts?: number;
 }
 
-const CartButton: React.FC<ICartButtonProps> = ({ qtyCartProducts = 0 }) => (
-  <Container>
-    {qtyCartProducts > 0 && (
-      <ContainerBadge>
-        <TextBadge>{qtyCartProducts}</TextBadge>
-      </ContainerBadge>
-    )}
-    <IconCart name="shopping-cart" size={24} color={themeGlobal.colors.gray2} />
-  </Container>
-);
+const CartButton: React.FC<ICartButtonProps> = ({ qtyCartProducts = 0 }) => {
+  const navigation = useNavigation();
+  return (
+    <Container onPress={() => navigation.navigate('Cart')}>
+      {qtyCartProducts > 0 && (
+        <ContainerBadge>
+          <TextBadge>{qtyCartProducts}</TextBadge>
+        </ContainerBadge>
+      )}
+      <IconCart
+        name="shopping-cart"
+        size={24}
+        color={themeGlobal.colors.gray2}
+      />
+    </Container>
+  );
+};
 
 export default CartButton;
