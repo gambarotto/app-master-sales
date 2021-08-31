0ff283ba1bab0de2a967a4b896d16e4a9205ca0f
gambarotto <dgcdeveloper@gmail.com>
2021-08-31 15:48:31 -0300

    add orderScreen

--- src/pages/OrderScreen/index.tsx
+++ src/pages/OrderScreen/index.tsx
@@ -1,8 +1,136 @@
+/* eslint-disable import/no-duplicates */
+import { useNavigation, useRoute } from '@react-navigation/native';
 import React from 'react';
-import { View } from 'react-native';
+import { format, formatDistance } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { ActivityIndicator } from 'react-native';
+import IconBack from '../../components/IconBack';
+import logoImage from '../../assets/logo_horizontal_white.png';
 
-// import { Container } from './styles';
+import {
+  Container,
+  ContainerHeader,
+  ContainerInfo,
+  ContainerItemOrderProduct,
+  ImageLogo,
+  ItemNameProduct,
+  ItemPriceProduct,
+  LineText,
+  OrderProductList,
+  TextOrder,
+  TextOrderDate,
+  TextTotal,
+  TextTotalCurrency,
+} from './styles';
+import { useFetch } from '../../hooks/useFetch';
+import { IAddress } from '../../contexts/auth';
+import { IProduct } from '../../contexts/products';
+import Currency from '../../components/Currency';
+import themeGlobal from '../../styles/global';
 
-const OrderScreen: React.FC = () => <View />;
+interface ResponseOrderDate {
+  dateDistance: string;
+  dateFormat: string;
+}
+interface IOrderId {
+  id: string;
+}
+interface ITransaction {
+  id: string;
+  status: string;
+  payment_method: string;
+  card_id: string;
+}
+export interface IProductOrder {
+  quantity: number;
+  price: number;
+  product: Omit<IProduct, 'categories'>;
+}
+
+interface IOrder {
+  id: string;
+  order_number: number;
+  amount: number;
+  delivery: boolean;
+  delivery_fee: number;
+  created_at: string;
+  updated_at: string;
+  transaction: ITransaction;
+  products: IProductOrder[];
+  billing: IAddress;
+  shipping: IAddress;
+}
+
+const OrderScreen: React.FC = () => {
+  const route = useRoute();
+  const { id: order_id } = route.params as IOrderId;
+  const navigation = useNavigation();
+  const { data: order } = useFetch<IOrder>(
+    `product-${order_id}`,
+    `orders/${order_id}`,
+  );
+
+  const orderDate = (): ResponseOrderDate | undefined => {
+    if (order) {
+      const dateOrder = new Date(order?.created_at);
+      const dateDistance = `( ${formatDistance(dateOrder, new Date(), {
+        addSuffix: true,
+        locale: ptBR,
+      })} )`;
+      const dateFormat = format(dateOrder, "dd 'de' MMMM 'de' yyyy", {
+        locale: ptBR,
+      });
+      return { dateDistance, dateFormat };
+    }
+    return undefined;
+  };
+  if (!order) {
+    return (
+      <Container>
+        <ActivityIndicator color={themeGlobal.colors.secondary} />
+      </Container>
+    );
+  }
+  return (
+    <Container>
+      <ContainerHeader>
+        <IconBack
+          styleElement={{ left: 16 }}
+          onPress={() => navigation.goBack()}
+          background
+        />
+        <ImageLogo source={logoImage} />
+      </ContainerHeader>
+      <ContainerInfo>
+        <TextOrder>{`Pedido #${order?.order_number}`}</TextOrder>
+        <TextOrderDate>{orderDate()?.dateFormat}</TextOrderDate>
+        <OrderProductList
+          data={order?.products}
+          keyExtractor={(item) => item.product.id}
+          renderItem={({ item }) => (
+            <ContainerItemOrderProduct key={item.product.id}>
+              <ItemNameProduct>{`${item.quantity} x ${item.product.name}`}</ItemNameProduct>
+              <ItemPriceProduct>
+                <Currency value={item.price} />
+              </ItemPriceProduct>
+            </ContainerItemOrderProduct>
+          )}
+        />
+        <LineText>
+          <TextTotal>Taxa de entrega</TextTotal>
+          <TextTotalCurrency>
+            <Currency value={order.delivery_fee} />
+          </TextTotalCurrency>
+        </LineText>
+        <LineText>
+          <TextTotal>Total</TextTotal>
+          <TextTotalCurrency>
+            <Currency value={order.amount} />
+          </TextTotalCurrency>
+        </LineText>
+      </ContainerInfo>
+    </Container>
+  );
+};
 
 export default OrderScreen;
--- src/pages/OrderScreen/styles.ts
+++ src/pages/OrderScreen/styles.ts
@@ -1,3 +1,97 @@
-import styled from 'styled-components/native';
+import Constants from 'expo-constants';
+import { FlatList } from 'react-native';
+import styled, { css } from 'styled-components/native';
+import { IProductOrder } from '.';
 
-export const Container = styled.View``;
+const { statusBarHeight } = Constants;
+
+export const Container = styled.View`
+  flex: 1;
+`;
+export const ContainerHeader = styled.View`
+  padding-top: ${`${statusBarHeight}px`};
+  ${(props) => css`
+    background-color: ${props.theme.colors.secondary};
+  `}
+  position: relative;
+  width: 100%;
+  height: 150px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding-left: 16px;
+  padding-right: 16px;
+`;
+export const ImageLogo = styled.Image`
+  height: 45px;
+  width: 160px;
+`;
+export const ContainerInfo = styled.View`
+  flex: 1;
+  width: 100%;
+  height: 100%;
+  ${(props) => css`
+    background-color: ${props.theme.colors.white};
+  `}
+  padding:20px;
+`;
+export const TextOrder = styled.Text`
+  ${(props) => css`
+    color: ${props.theme.colors.primary};
+  `}
+  font-size:18px;
+  font-family: 'Roboto-Bold';
+  margin-bottom: 8px;
+`;
+export const TextOrderDate = styled.Text`
+  ${(props) => css`
+    color: ${props.theme.colors.primary};
+  `}
+  font-size:14px;
+  font-family: 'Roboto-Regular';
+  margin-bottom: 16px;
+`;
+export const OrderProductList = styled(
+  FlatList as new () => FlatList<IProductOrder>,
+)``;
+export const ContainerItemOrderProduct = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 4px;
+  padding-left: 2px;
+`;
+export const ItemNameProduct = styled.Text`
+  ${(props) => css`
+    color: ${props.theme.colors.gray3};
+  `}
+  font-size:12px;
+  font-family: 'Roboto-Regular';
+`;
+export const ItemPriceProduct = styled.Text`
+  ${(props) => css`
+    color: ${props.theme.colors.gray3};
+  `}
+  font-size:13px;
+  font-family: 'Roboto-Regular';
+`;
+export const LineText = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 13px;
+`;
+export const TextTotal = styled.Text`
+  ${(props) => css`
+    color: ${props.theme.colors.gray2};
+  `}
+  font-size:14px;
+  font-family: 'Roboto-Bold';
+`;
+export const TextTotalCurrency = styled.Text`
+  ${(props) => css`
+    color: ${props.theme.colors.gray2};
+  `}
+  font-size:14px;
+  font-family: 'Roboto-Bold';
+`;
